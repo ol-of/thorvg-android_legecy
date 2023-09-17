693ce2c1c3d29e67b6f6a2b24f135b3df09c0d39
ol-of <81972014+ol-of@users.noreply.github.com>
2023-09-17 23:52:33 +0900

    add 'app' module

---
+++ app/src/main/java/com/thorvg/sample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.thorvg.sample;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.thorvg.android.widget.LottieView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LottieView lottieView = findViewById(R.id.lottieView);
+        lottieView.setJsonFilePath("swinging.json");
+    }
+}
