b8fc57c055229792744bdfbca3134185ae39ea75
Youjin Lee <ol-of@naver.com>
2023-09-18 05:34:45 +0900

    Change LottieDrawable's parent class

--- thorvg/src/main/java/com/thorvg/android/graphics/drawable/LottieDrawable.java
+++ thorvg/src/main/java/com/thorvg/android/graphics/drawable/LottieDrawable.java
@@ -2,10 +2,17 @@ package com.thorvg.android.graphics.drawable;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.drawable.Animatable;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.thorvg.android.NativeLib;
 
@@ -13,10 +20,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-public class LottieDrawable extends BitmapDrawable implements Animatable {
-    private final Context mContext;
-    private final Bitmap mBitmap;
-    private final String mJsonFilePath;
+public class LottieDrawable extends Drawable implements Drawable.Callback, Animatable {
+    private static final String LOGTAG = LottieDrawable.class.getSimpleName();
+
+    private LottieDrawableState mLottieState;
+    private Context mContext;
+    private Bitmap mBitmap;
+    private String mJsonFilePath;
     private int mWidth;
     private int mHeight;
 
@@ -28,12 +38,28 @@ public class LottieDrawable extends BitmapDrawable implements Animatable {
         mBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
     }
 
+
+    /**
+     * The one constructor to rule them all. This is called by all public
+     * constructors to set the state and initialize local properties.
+     */
+    private LottieDrawable(@Nullable LottieDrawableState state, @Nullable Resources res) {
+        // As the mutable, not-thread-safe native instance is stored in LottieDrawableState, we
+        // need to always do a defensive copy even if mutate() isn't called. Otherwise
+        // draw() being called on 2 different LottieDrawable instances could still hit the same
+        // underlying native object.
+        mLottieState = new LottieDrawableState(state);
+//        updateLocalState(res);
+    }
+
     @Override
     public void start() {
+
     }
 
     @Override
     public void stop() {
+
     }
 
     @Override
@@ -41,11 +67,49 @@ public class LottieDrawable extends BitmapDrawable implements Animatable {
         return false;
     }
 
+//    @Override
+//    public ConstantState getConstantState() {
+//        mLottieState.mChangingConfigurations = getChangingConfigurations();
+//        return mLottieState;
+//    }
+
     @Override
-    public void draw(Canvas canvas) {
+    public void draw(@NonNull Canvas canvas) {
         String jsonContent = loadJSONFromAsset(mContext, mJsonFilePath);
         NativeLib.nRenderLottie(jsonContent, jsonContent.length(), mBitmap, mWidth, mHeight);
-        canvas.drawBitmap(mBitmap, 0, 0, getPaint());
+        canvas.drawBitmap(mBitmap, 0, 0, new Paint());
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        // We can't tell whether the drawable is fully opaque unless we examine all the pixels,
+        // but we could tell it is transparent if the root alpha is 0.
+        return getAlpha() == 0 ? PixelFormat.TRANSPARENT : PixelFormat.TRANSLUCENT;
+    }
+
+    @Override
+    public void invalidateDrawable(@NonNull Drawable who) {
+
+    }
+
+    @Override
+    public void scheduleDrawable(@NonNull Drawable who, @NonNull Runnable what, long when) {
+
+    }
+
+    @Override
+    public void unscheduleDrawable(@NonNull Drawable who, @NonNull Runnable what) {
+
     }
 
     private static String loadJSONFromAsset(Context context, String fileName) {
@@ -67,5 +131,32 @@ public class LottieDrawable extends BitmapDrawable implements Animatable {
             e.printStackTrace();
         }
         return json;
+    }
+
+    static class LottieDrawableState extends ConstantState {
+        int mChangingConfigurations;
+
+        public LottieDrawableState(LottieDrawableState copy) {
+            if (copy != null) {
+                mChangingConfigurations = copy.mChangingConfigurations;
+            }
+        }
+
+        @NonNull
+        @Override
+        public Drawable newDrawable() {
+            return new LottieDrawable(this, null);
+        }
+
+        @NonNull
+        @Override
+        public Drawable newDrawable(Resources res) {
+            return new LottieDrawable(this, res);
+        }
+
+        @Override
+        public int getChangingConfigurations() {
+            return mChangingConfigurations;
+        }
     }
 }
